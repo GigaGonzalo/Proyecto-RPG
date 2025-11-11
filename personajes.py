5e2ffc789df8e7d6a1f556b9b251bab7c7c14cc6
GigaGonzalo <darkorbitb1@live.com.mx>
2025-11-11 12:55:51 -0600

    feat: Añade clase guerrero y funciones especificas

--- personajes.py
+++ personajes.py
@@ -6,22 +6,19 @@ class Personaje:
     armadura = 0
     escudoM = 0
 
-    def __init__(self, vida, energia, ataque, armadura, escudoM):
+    def __init__(self, vida, energia, ataque, armadura, escudo_mag, costo_mov):
         self.vida = vida
         self.energia = energia
         self.ataque = ataque
         self.armadura = armadura
-        self.escudoM = escudoM
+        self.escudo_mag = escudo_mag
+        self.costo_mov = costo_mov
 
 
     def curar_Vida(self, cura_mas):
         self.vida += cura_mas
         return self.vida
-    
-    def incrementar_Armadura(self, arma_mas):
-        self.armadura += arma_mas
-        return self.armadura
-    
+
     def crear_EscudoM(self, escudo_mas):
         self.escudoM += escudo_mas
         return self.escudoM
@@ -29,8 +26,8 @@ class Personaje:
     def restaurar_Energia(self, energia_mas):
         self.energia += energia_mas
 
-    def aplicar_Defensa(self, daño_puro):
-        daño_mitigado = daño_puro - (self.armadura + self.escudoM)
+    def aplicar_Defensa(self, daño_puro, bonificacion):
+        daño_mitigado = daño_puro - (self.armadura + self.escudoM + bonificacion)
         return daño_mitigado
 
     def recibir_Ataque(self, daño_recibido):
@@ -40,17 +37,57 @@ class Personaje:
         self.ataque += mod_daño
         return self.ataque
 
-    def movimiento(self,costo):
-        self.energia -= costo
+    def movimiento(self, penalizacion):
+        self.energia -= self.costo_mov + penalizacion
 
 
-persona = Personaje(10,10,10,10,10)
+persona = Personaje(10,10,10,10,10, 1)
 print(f"La persona tiene de vida :{persona.vida}")
 print(f"La persona tiene de energia :{persona.energia}")
 print(f"La persona tiene de ataque :{persona.ataque}")
 print(f"La persona tiene de armadura :{persona.armadura}")
 print(f"La persona tiene de escudo magico :{persona.escudoM}")
 persona.recibir_Ataque(5)
-persona.movimiento(3)
+persona.movimiento(2)
 print(f"La persona se tropezo y ahora tiene {persona.vida} de vida "
       + f"tambien termino con {persona.energia} de energia")
+
+class Guerrero(Personaje):
+
+    def __init__(self, vida, energia, ataque, armadura, escudo_mag, costo_mov,
+                  habilidad_guerrero, bono_armadura):
+        super().__init__(vida, energia, ataque, armadura, escudo_mag, costo_mov)
+        
+        self.habilidad_guerrero = habilidad_guerrero
+        self.bono_armadura = bono_armadura
+
+    def habilidad_Guerrero_Ataque(self):
+        h_ataque = self.ataque * self.bono_armadura
+        return h_ataque
+    
+    def habilidad_Guerrero_Defensa(self):
+        h_defensa = self.bono_armadura * 3
+        return h_defensa
+
+vidaRada = 5
+ador = Guerrero(10,10,12,3,0,1,2,1)
+print(f"ADOR tiene de vida :{ador.vida}")
+print(f"ADOR tiene de energia :{ador.energia}")
+print(f"ADOR tiene de ataque :{ador.ataque}")
+print(f"ADOR tiene de armadura :{ador.armadura}")
+print(f"ADOR tiene de escudo magico :{ador.escudoM}")
+print(f"ADOR tiene de puntos de habilidad : {ador.habilidad_guerrero} ")
+print(f"ADOR tiene una bonificacion de {ador.bono_armadura}")
+
+print(f"El guerrero Ador es atacado por una rata a la que le piso la cola!!")
+ador.recibir_Ataque(ador.aplicar_Defensa(5, ador.bono_armadura))
+print(f"Ador ahora tiene {ador.vida} puntos de vida y ahora el ataca con su espada")
+vidaRada = vidaRada - ador.causar_Daño(0)
+if vidaRada <= 0:
+    print(f"La rata a muerto a causa del ataque")
+    ador.movimiento(0)
+    print(f"Ador a perdido {ador.costo_mov} de energia, tiene ahora {ador.energia}")
+elif vidaRada > 0:
+    print(f"La rata furiosa regresa el ataquecon una mordida")
+    ador.recibir_Ataque(ador.aplicar_Defensa(5, ador.bono_armadura))
+    print(f"Ador ahora tiene {ador.vida} puntos de vida")
